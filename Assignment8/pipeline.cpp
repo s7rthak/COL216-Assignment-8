52cc820268b7f2cb43f37b7c8237913a4fcfe6af
As4s1n <behera.sarthak3@gmail.com>
2020-08-10 13:35:45 +0530

    added some tests for assignment-8

--- Assignment8/pipeline.cpp
+++ Assignment8/pipeline.cpp
@@ -1,25 +1,6 @@
-#include <bits/stdc++.h>
-#include <execinfo.h>
-#include <unistd.h>
-#include "instruction_read.hpp"
-#include "memory_read.hpp"
-#include "util.hpp"
 #include "pipeline.hpp"
-typedef long long ll;
-using namespace std;
+#include "util.hpp"
 
-void handler(int sig) {
-    void *array[10];
-    size_t size;
-
-    // get void*'s for all entries on the stack
-    size = backtrace(array, 10);
-
-    // print out all the frames to stderr
-    fprintf(stderr, "Error: signal %d:\n", sig);
-    backtrace_symbols_fd(array, size, STDERR_FILENO);
-    exit(1);
-}
 
 void handleIF(Pipeline& mips, vector<string>& inst_mem){
     PipeStage& IF = mips.instructionFetch;
@@ -201,77 +182,5 @@ void checkForStall(Pipeline& mips){
         mips.isStalled = true;
     }else{
         mips.isStalled = false;
-    }
-}
-
-int main(int argc, char *argv[]){
-    // signal(SIGSEGV, handler);
-    Pipeline MIPS;
-    vector<string> instructionMemory = instructionsFileToVector(argv[1]);
-    vector<int> registerFile(32);
-    vector<int> Memory = memoryFileToVector(argv[2]);
-    registerFile[0] = 1;
-    registerFile[1] = 2;
-    int i=0;
-    while(MIPS.instructionRead < (signed)instructionMemory.size()){
-        string pipe;
-        checkForStall(MIPS);
-        handleWB(MIPS, registerFile);
-        pipe = to_string(MIPS.writeBack.PC/4) + " " + pipe;
-        handleMEM(MIPS, Memory);
-        pipe = to_string(MIPS.memoryAccess.PC/4) + " " + pipe;
-        handleEX(MIPS);
-        pipe = to_string(MIPS.executeInstruction.PC/4) + " " + pipe;
-        handleID(MIPS, registerFile);
-        pipe = to_string(MIPS.instructionDecode.PC/4) + " " + pipe;
-        handleIF(MIPS, instructionMemory);
-        pipe = to_string(MIPS.instructionFetch.PC/4) + " " + pipe;
-
-        for(int i=0; i<registerFile.size(); i++)
-        {
-            cout << "$" << i << " " << registerFile[i] << endl;
-        }
-        cout << endl << endl;
-
-        for(int i=0;i<Memory.size(); i++)
-        {
-            cout << "Memory[" << i << "]: " << Memory[i] << endl;
-        }
-        cout << "Pipeline:"  << "\n";
-        cout << pipe << "\n\n";
-        MIPS.clock++;
-    }
-
-    // Ending Statements
-    for(int i=0; i<4; i++)
-    {
-        string pipe;
-        if(i<4)
-            handleWB(MIPS, registerFile);
-        pipe = to_string(MIPS.writeBack.PC/4) + " " + pipe;
-        if(i<3)
-            handleMEM(MIPS, Memory);
-        pipe = to_string(MIPS.memoryAccess.PC/4) + " " + pipe;
-        if(i<2)
-            handleEX(MIPS);
-        pipe = to_string(MIPS.executeInstruction.PC/4) + " " + pipe;
-        if(i < 1)
-            handleID(MIPS, registerFile);
-        pipe = to_string(MIPS.instructionDecode.PC/4) + " " + pipe;
-        pipe = to_string(MIPS.instructionFetch.PC/4) + " " + pipe;
-
-        for(int i=0; i<registerFile.size(); i++)
-        {
-            cout << "$" << i << " " << registerFile[i] << endl;
-        }
-        cout << endl << endl;
-
-        for(int i=0;i<Memory.size(); i++)
-        {
-            cout << "Memory[" << i << "]: " << Memory[i] << endl;
-        }
-        cout << "Pipeline:"  << "\n";
-        cout << pipe << "\n\n";
-        MIPS.clock++;
     }
 }
--- Assignment8/pipeline_test.cpp
+++ Assignment8/pipeline_test.cpp
@@ -1,16 +1,28 @@
 #include <gtest/gtest.h>
-#include "pipeline.hpp"
 #include <bits/stdc++.h>
+#include "pipeline.hpp"
 
 using namespace std;
 
-vector<int>reg(32);
-vector<int>mem(100, 3);
-vector<string>inst_mem;
+TEST(MIPS, Stall_On_Data_Hazard_1){
+    Pipeline P;
+    P.instructionDecode.Rs = 1; P.executeInstruction.Rd = 1; P.executeInstruction.RegWrite = 1;
+    checkForStall(P);
+    ASSERT_EQ(P.isStalled, true);
+}
 
+TEST(MIPS, Stall_On_Data_Hazard_2){
+    Pipeline P;
+    P.writeBack.Rd = 1; P.executeInstruction.Rs = 1; P.writeBack.RegWrite = 1;
+    checkForStall(P);
+    ASSERT_EQ(P.isStalled, true);
+}
 
-TEST(MIPS, Stall){
-
+TEST(MIPS, Stall_On_Data_Hazard_3){
+    Pipeline P;
+    P.writeBack.Rt = 1; P.executeInstruction.Rs = 1; P.writeBack.MemtoReg = 1;
+    checkForStall(P);
+    ASSERT_EQ(P.isStalled, true);
 }
 
 int main(int argc, char **argv) {
